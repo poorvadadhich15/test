fe5266da80909134892bd52eeb9196fc45498a7b
poorvadadhich15 <84984060+poorvadadhich15@users.noreply.github.com>
2021-06-21 13:23:09 +0530

    Update user.js

--- user.js
+++ user.js
@@ -1,6 +1,4 @@
 const mongoose = require("mongoose");
-//mngoose is class schema types there dataypes which are only present in mongo db,
-//or list of objects which is present in mongo db
 const User = new mongoose.Schema({
   id:{
     type:mongoose.SchemaTypes.ObjectId,
